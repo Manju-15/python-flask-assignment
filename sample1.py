98dfcff8e1e8e4c951e5249ab06ac10df41e5378
Manju-15 <srinimanju09@gmail.com>
2022-10-24 15:06:21 +0530

    dummy changes

--- sample1.py
+++ sample1.py
@@ -6,8 +6,12 @@ test_tuple1 = (4, 5)
 test_tuple2 = (7, 8)
 # printing original tuples
 print("The original tuple 1 : " + str(test_tuple1))
-print("The original tuple 2 : " + str(test_tuple2))
-# All pair combinations of 2 tuples
+print("The updated tuple 2 : " + str(test_tuple2))
+# All pair s of 2 tuples
+asdfasdfas
+asdfasdfasasdfasdf
+dfsas
+dfsasdf
 # Using list comprehension
 res = [(a, b) for a in test_tuple1 for b in test_tuple2]
 res = res + [(a, b) for a in test_tuple2 for b in test_tuple1]
